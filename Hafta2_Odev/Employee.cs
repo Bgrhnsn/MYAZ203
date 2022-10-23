c9ec6fc55abf10cbafed30d33d56daf8a5c9a36f
Bgrhnsn <116509363+Bgrhnsn@users.noreply.github.com>
2022-10-23 19:53:20 +0300

    Add files via upload

---
+++ Hafta2_Odev/Employee.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hafta2_Odev
+{
+    public class Employee
+    {
+        public int Id { get; set; } 
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public List<Employee> list = new List<Employee>();
+        public Employee()
+        {
+            
+        }
+       public  Employee(int id,string fn,string ln)
+        {
+            Id = id;
+            FirstName = fn;
+            LastName = ln;
+        }
+
+        
+        
+
+        public void Add(Employee employee)
+        {
+            list.Add(employee);
+
+            Console.WriteLine(Id +" Numarali Isci Eklendi...");
+        }
+
+        public void Remove(Employee employee , int a)
+        {
+            if(a==Id)
+            {
+                list.Remove(employee);
+                Console.WriteLine(Id + " Numarali Isci Cikarildi...");
+            }
+            else
+            {
+                Console.WriteLine("Bu Id'e Sahip Bir Isci Bulunmamaktadır...");
+            }
+        }
+
+       
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} {Id}";
+        }
+
+        
+    }
+}
---
+++ Hafta2_Odev/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hafta2_Odev
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee
+            {
+                FirstName = "Bugra",
+                LastName = "Sen",
+                Id = 221118075
+            };
+            employee.Add(employee);
+
+            Employee employee2 = new Employee
+            {
+                FirstName = "Kerem",
+                LastName = "Dag",
+                Id = 223232512
+            };
+            employee2.Add(employee2);
+
+            employee.Remove(employee, 221118075);
+           
+
+            
+            Console.ReadLine();
+
+        }
+    }
+}
