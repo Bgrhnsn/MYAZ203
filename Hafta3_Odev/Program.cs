bcb10494c4c15e3bf75a29721ccbd1739ce8a849
Bgrhnsn <116509363+Bgrhnsn@users.noreply.github.com>
2022-10-23 19:54:27 +0300

    Add files via upload

---
+++ Hafta3_Odev/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hafta3_Odev
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Product urun_1 = new Product() { Price = 150, ProductId = 1, ProductName = "Yag" };
+            Product urun_2 = new Product() { Price = 60, ProductId = 2, ProductName = "Un" };
+            Product urun_3 = new Product() { Price = 80, ProductId = 3, ProductName = "Seker" };
+            Product urun_4 = new Product() { Price = 100, ProductId = 4, ProductName = "Pirinc" };
+
+
+
+            CartItem item1 = new CartItem() { CartId = 1, Product = urun_1, Quantity = 2 };
+            CartItem item2 = new CartItem() { CartId = 2, Product = urun_2, Quantity = 2 };
+            CartItem item3 = new CartItem() { CartId = 3, Product = urun_3, Quantity = 5 };
+            CartItem item4 = new CartItem() { CartId = 4, Product = urun_4, Quantity = 10 };
+
+
+            ShoppingCart liste = new ShoppingCart();
+            
+            liste.Add(item1);
+            liste.Add(item2);
+            liste.Add(item3);
+            liste.Add(item4);
+
+            liste.GetBalance();
+            liste.Getlist();
+
+            Console.WriteLine(liste.GetBalance());
+
+            liste.Remove(2);
+           
+            liste.GetBalance();
+            liste.Getlist();
+            
+
+            Console.WriteLine(liste.GetBalance());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Hafta3_Odev/ShoppingCart.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hafta3_Odev
+{
+    public class ShoppingCart
+    {
+       public  List<CartItem> products;
+
+        public ShoppingCart()
+        {
+            products = new List<CartItem>();    
+        }
+        public ShoppingCart(CartItem products)
+        {
+            Add(products);
+        }
+        public void Add(CartItem productsa)
+        {
+            products.Add(productsa);
+           
+        }
+        public string GetBalance()
+        {
+            double price = 0;
+            foreach (var item in products)
+            {
+                price += item.Product.Price * item.Quantity;
+            }
+            return ($"Toplam Tutar : {price}");
+
+        }
+        public void Getlist()
+        {
+            foreach (var item in products)
+                Console.WriteLine(item);
+        }
+        public void Remove(int Id)
+        {
+            var removed = products.FirstOrDefault(a => a.CartId == Id);
+            if (removed == null)
+            {
+                Console.WriteLine("Ardadiginiz Urun Bulunamadi...");
+                return;
+            }
+            products.Remove(removed);
+            Console.WriteLine(Id+" Numarali Urun Cikarildi...");
+        }
+
+    }
+}
